6cf062ce7be9ac84092f1edba8adfe2e07dacf7d
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2025-05-08 11:42:51 -0400

    Added `Wong (2011)` citation
    Wong, B. Points of view: Color blindness. Nat Methods 8, 441 (2011). https://doi.org/10.1038/nmeth.1618

--- cat_colors.m
+++ cat_colors.m
@@ -49,8 +49,8 @@ function C = cat_colors(palette,N)
 % turku      100x3    ibid.
 %
 % bold        10x3    Liu (2024)
-% colorblind   6x3    ibid.    
-% prism       10x3    ibid.
+% colorblind   6x3    Wong (2011)   
+% prism       10x3    Liu (2024)
 % tab         10x3    ibid.
 % tab20       20x3    ibid.
 % vivid       10x3    ibid.
@@ -77,6 +77,8 @@ function C = cat_colors(palette,N)
 % Liu, Z. (2024). 200 colormap. MATLAB Central File Exchange. 
 %   https://mathworks.com/matlabcentral/fileexchange/120088. Retrieved 
 %   November 2, 2024.
+% Wong, B. Points of view: Color blindness. Nat Methods 8, 441 (2011). 
+%   https://doi.org/10.1038/nmeth.1618
 %
 %See also
 % cat_colorbar
@@ -159,7 +161,7 @@ switch palette
     % ================================================================== %
     case 'bold'
         user_palette = categorical_palettes.bold;
-    case 'colorblind'
+    case {'colorblind','wong'} % See https://doi.org/10.1038/nmeth.1618
         user_palette = categorical_palettes.colorblind;
     case {'prism2','prism'}
         user_palette = categorical_palettes.prism2;
@@ -180,6 +182,20 @@ switch palette
                         0.3137 0.9804 0.4824;...
                         0.9451 0.9804 0.5490;...
                         1.0000 0.3333 0.3333];
+    % ================================================================== %
+    % Ohio State color palette (Easter egg, not documented)
+    % ================================================================== %
+    case {'ohiostate','osu'} % Easter egg: not documented
+        user_palette = [0.4510 0.4824 0.4941
+                        0.5137 0.0000 0.3961
+                        0.4314 0.7333 0.6706
+                        0.6902 0.2706 0.3451
+                        1.0000 0.7137 0.0000
+                        0.0549 0.2941 0.3216
+                        0.9020 0.3725 0.2000
+                        0.5020 0.7804 0.3569
+                        0.2314 0.6431 0.8118
+                        0.4078 0.2627 0.1529];
     otherwise
         error('Palette not recognized.')
 end
